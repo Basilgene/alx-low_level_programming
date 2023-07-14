f466d56a26f5aaedadeea8c370596547f59cbbc0
Basilgene <genevievebasil5@gmail.com>
2023-07-13 19:37:43 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - This program prints using the printf function
- * Return: 0 (Sucess)
+ * Return: 0 
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
