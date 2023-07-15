a08ffed87edd7ff8355825c1e4dd4d7dc7575783
Basilgene <genevievebasil5@gmail.com>
2023-07-14 17:28:51 -0700

    add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This program prints lower case followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
