165594dbd53b251d20cf96fa2a2af98399cd73b3
Basilgene <genevievebasil5@gmail.com>
2023-07-19 15:33:02 -0700

    add alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	alphabet = 'a'
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+}
