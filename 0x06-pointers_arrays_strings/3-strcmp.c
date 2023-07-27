897545c96837eae7c110232d4bf5ccb022321e29
Basilgene <genevievebasil5@gmail.com>
2023-07-27 03:06:12 -0700

    compare 2 string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  _strcmp - compare two string
+ *  @s1: input 1
+ *  @s2: input 2
+ *  Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+
+	j = 0;
+	while (s1[j] != '\0' && s2[j] != '\0')
+	{
+		if (s1[j] != s2[j])
+		{
+			return (s1[j] - s2[j]);
+		}
+		j++;
+	}
+	return (0);
+}
