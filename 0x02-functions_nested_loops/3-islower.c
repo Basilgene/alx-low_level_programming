811b1356a626d7e39047cd5b69ae58305c6016d7
Basilgene <genevievebasil5@gmail.com>
2023-08-04 05:32:44 -0700

    upper and lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,21 @@
 #include "main.h"
 /**
- * _islower - detect lowercase and uppercase
+ * _islower - checks lowercase and uppercase
  * Description: use _putchar to print
+ * @c: collect alphabet
  * Return: 1 and 0
  */
 int _islower(int c)
 {
+	char j;
+	int lower = 0;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		if (j == c)
+		{
+		lower = j;
+		}
+	}
+	return (lower);
 }
