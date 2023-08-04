d41766935bb9b0ec69f12b0b66e8f9a028340f9f
Basilgene <genevievebasil5@gmail.com>
2023-08-04 14:48:22 -0700

    name and new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print its name and a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
