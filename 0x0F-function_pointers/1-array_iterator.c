97c0b1f8d9beb42144549f05b5296611d7733618
Basilgene <genevievebasil5@gmail.com>
2023-08-16 05:28:12 -0700

    executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function
+ * @size: size of array
+ * @action: a pointer to function
+ * Return: 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
